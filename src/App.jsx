548eb1620ec0c18d262c765372e8462f065807a3
dubstabber <taesan@protonmail.com>
2025-03-25 16:08:21 +0100

    Make styled components more reusable, initial react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-
-const StyledApp = styled.div`
-  background-color: orangered;
-  padding: 20px;
-`;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert("Check in")}>Check in</Button>
-        <Button onClick={() => alert("Check out")}>Check out</Button>
-
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
-      </StyledApp>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashboard" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
