f8dd85a400cf50e5afcf777243113e2b3ba0baef
dubstabber <taesan@protonmail.com>
2025-05-06 21:42:17 +0200

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
@@ -33,7 +33,7 @@ function App() {
         {/* <ReactQueryDevtools initialIsOpen={false} /> */}
 
         <GlobalStyles />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route
               element={
@@ -55,7 +55,7 @@ function App() {
             <Route path="login" element={<Login />} />
             <Route path="*" element={<PageNotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
 
         <Toaster
           position="top-center"
